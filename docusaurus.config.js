090042e18c38ba73ec14442545ff441166888971
Balthasar Hofer <lebalz@outlook.com>
2022-01-01 21:36:20 +0100

    addlogo

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -46,7 +46,7 @@ const config = {
         title: 'Dev Blog',
         logo: {
           alt: 'Le Balz',
-          src: 'img/logo.svg',
+          src: 'img/logo.png',
         },
         items: [
           {
