7c82eeb3ef73718da5e2f3cd380314193e0ae939
Balthasar Hofer <lebalz@outlook.com>
2022-11-17 14:41:39 +0100

    ardui ino

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -124,7 +124,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['bash', 'powershell', 'ruby', 'arduino'],
+        additionalLanguages: ['bash', 'powershell', 'ruby', 'arduino', 'ino'],
       },
     }),
   plugins: [
